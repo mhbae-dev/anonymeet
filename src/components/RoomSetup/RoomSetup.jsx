32c3182d49fc111567291a0a6c813ff3963d3e05
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-17 14:54:56 +0100

    Updated default date to dd/mm/yyy on RoomSetup.jsx

--- src/components/RoomSetup/RoomSetup.jsx
+++ src/components/RoomSetup/RoomSetup.jsx
@@ -15,7 +15,7 @@ const RoomSetup = (props) => {
   let mm = String(today.getMonth() + 1).padStart(2, '0');
   let yyyy = today.getFullYear();
   
-  today = mm + '/' + dd + '/' + yyyy;
+  today = dd + '/' + mm + '/' + yyyy;
 
   const [startDate, setStartDate] = useState(today);
   const [endDate, setEndDate] = useState(today);
