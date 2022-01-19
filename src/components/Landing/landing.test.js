18ec2efd79d4b7ebd432fa59c0706a37df440073
88Andybird <anbird88@gmail.com>
2022-01-19 11:04:57 +0000

    Edited tests to reflect app changes

--- src/components/Landing/landing.test.js
+++ src/components/Landing/landing.test.js
@@ -4,12 +4,12 @@ import App from '../../App';
 
 it("renders App component correctly", () => {
   const { getByText } = render(<App />);
-  expect(getByText(/Welcome to Anonymeet/i)).toBeInTheDocument();
+  expect(getByText(/Welcome to/i)).toBeInTheDocument();
 });
 
 it("create new event button is present on the page", () => {
   const { getByRole } = render(<App />);
-  expect(getByRole('button', {name: /Create New Event/i})).toBeInTheDocument();
+  expect(getByRole('button', {name: /Start/i})).toBeInTheDocument();
 });
 
 test('calls onClick prop when clicked', () => {
@@ -17,7 +17,7 @@ test('calls onClick prop when clicked', () => {
     <button onClick={onClick}>{children}</button>
   );
   const handleClick = jest.fn();
-  render(<Button onClick={handleClick}>Create New Event</Button>);
-  fireEvent.click(screen.getByText(/Create New Event/i));
+  render(<Button onClick={handleClick}>Start</Button>);
+  fireEvent.click(screen.getByText(/Start/i));
   expect(handleClick).toHaveBeenCalledTimes(1);
 });
